ffff4a0265833c62c7fd07f005bdde4358dcedc8
dirai <DipakRai@users.noreply.github.com>
2019-04-12 17:40:18 +0530

    module2-solution uploaded for week 2 assignment
    module2-solution uploaded for week 2 assignment

---
+++ module2-solution/app.js
@@ -0,0 +1,89 @@
+(function () {
+  'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+
+function ToBuyController(ShoppingListCheckOffService) {
+  var toBuyList = this;
+  toBuyList.itemName = "";
+  toBuyList.itemQuantity = "";
+  toBuyList.populateToBuyList = function(){
+  var itemsToBuy = [
+          { itemName: 'Cookies', itemQuantity: 10},
+          { itemName: 'Cakes', itemQuantity: 5},
+          { itemName: 'Ice-Creams', itemQuantity: 7},
+          { itemName: 'Milk', itemQuantity: 8},
+          { itemName: 'Bread', itemQuantity: 9}
+        ];
+        toBuyList.itemsToBuy=itemsToBuy;
+  }
+  toBuyList.populateToBuyList();
+
+  toBuyList.addItemsToBoughtList = function (item){
+    ShoppingListCheckOffService.addItem(item.itemName,item.itemQuantity);
+    toBuyList.removeItem(item);
+  };
+
+  toBuyList.removeItem = function(item){
+    var index = toBuyList.itemsToBuy.indexOf(item);
+    toBuyList.itemsToBuy.splice(index, 1);
+  };
+
+  toBuyList.errorMessage = function(){
+    if(toBuyList.itemsToBuy.length==0){
+      return true;
+    } else {
+      return false;
+    }
+  };
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var boughtList = this;
+  boughtList.itemName = "";
+  boughtList.itemQuantity = "";
+  boughtList.itemsBought=[];
+  boughtList.itemsBought = ShoppingListCheckOffService.getItems();
+  boughtList.removeItem = function (itemIndex) {
+    ShoppingListCheckOffService.removeItem(itemIndex);
+  };
+
+  boughtList.errorMessage = function(){
+    if(boughtList.itemsBought.length==0){
+      alert('boughtList.errorMessage '+true);
+      return true;
+    } else {
+      return false;
+    }
+  };
+}
+
+
+function ShoppingListCheckOffService() {
+  var service = this;
+  // List of shopping items
+  var bought = [];
+  service.bought=[];
+  service.addItem = function (itemName, quantity) {
+    var item = {
+      itemName: itemName,
+      itemQuantity: quantity
+    };
+    service.bought.push(item);
+    alert(service.bought);
+  };
+  /*service.removeItem = function (itemIndex) {
+    bought.splice(itemIndex, 1);
+  };*/
+  service.getItems = function () {
+    alert('service.bought ='+service.bought);
+    return service.bought;
+  };
+}
+})();
